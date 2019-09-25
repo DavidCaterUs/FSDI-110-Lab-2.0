afb19c693f470d74bbe0ba825fa6b1c513ab77a4
David <roberto@localhost.localdomain>
2019-09-25 20:58:15 -0300

    lab 3

--- wwwroot/js/catalog.js
+++ wwwroot/js/catalog.js
@@ -27,9 +27,15 @@ function getCatalogData() {
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button type="button" class="btn btn-primary">Rent Now!</button>
+            <h5 class="card-title">Car Rental!</h5>
+              <ul>
+                <li>${car.Make}</li>
+                <li>${car.Model}</li>
+                <li>${car.Year}</li>
+                <li>${car.Color}</li>
+                <li>${car.DailyPrice}</li>
+              </ul>
+              <button type="button" class="btn btn-primary">Rent Now!</button>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
